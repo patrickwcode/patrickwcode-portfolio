93742c4768ffd45cdaa53393051165a692cd4de8
Patrick Wauschek <patrick@patrickwcode.com>
2024-06-12 08:56:50 -0500

    Updates About page text and text box.

--- src/components/About.js
+++ src/components/About.js
@@ -2,42 +2,53 @@ import React from "react";
 
 export default function About() {
   return (
-    <section id="about" className="w-full h-full lg:h-screen py-[18vh] bg-[rgb(209,220,223)]">
+    <section
+      id="about"
+      className="w-full h-full lg:h-screen py-[18vh] bg-[rgb(209,220,223)]"
+    >
       <h1 className="title-font sm:text-4xl text-3xl font-medium text-center">
         Hello, Earthling.
       </h1>
-      <p className="text-xl leading-relaxed p-8">
+      <p className="text-xl leading-relaxed p-8 ml-8 mr-8 sm:ml-32 sm:mr-32 md:ml-44 md:mr-44 lg:ml-60 lg:mr-60 bg-white/60 rounded-3xl">
         Since the age of 7, I have been fascinated with computers and
         programming. My neighbor was a computer wizard, who opened the portal to
         this evolving passion. I started making index cards for terms such as
-        CPU, RAM, and ROM to memorize for fun. I was able to attach an image to
-        these components when he showed me his top of the line gaming rig. I
-        knew right then and there that I had to build a computer for myself one
-        day.
+        CPU, RAM, and ROM to memorize for fun. Later, I was able to attach a
+        mental image to these components when he showed me his top-of-the-line
+        gaming rig. I knew right then and there that I had to build a computer
+        for myself one day.
         <br></br>
         <br></br>
-        Fast forward to college in 2010. My dream has come true and I am
-        pursuing my other dream of graduating with a Bachelor's in Film and
-        Computer Science. Java was a required language to learn and I didn't
-        grasp it very easily. My interest in programming plateaued due to this.
-        Editing and graphic design slowly became the front runner for me.
-        <br></br>
-        <br></br>I graduated the following year in spring and was ready to work.
-        I decided to look for graphic design jobs since I felt more confident in
-        this field. In 2015, I landed my dream job of working for NBC Arthouse
-        as a graphic designer. Throughout the years, I have fine tuned my craft
-        and expanded into many new areas. One of them being ReactJS.
-        <br></br>
-        <br></br>I took it upon myself to learn JavaScript and React and apply
-        it to the InTouch touchscreen system that on-air talent use. JavaScript
-        felt much more natural to me, especially not having to worry about
-        compiling code constantly. Eventually, I started building apps to help
-        cover topics like the FIFA World Cup and weather. The blend of design
-        and programming put me in total bliss.
+        Fast forward to 2011. I checked 2 items off of my bucket list. I built
+        my gaming computer and I graduated from Towson University with a
+        Bachelor's in Electronic Media & Film and Computer Science. For the next
+        2 years, I did small freelance gigs involving graphic design and motion
+        graphics. In November 2013, I got the biggest job of my career at NBC
+        Arthouse, which later turned into my current full-time career in June
+        2015.
         <br></br>
         <br></br>
-        Instead of choosing between my two passions, why not combine them as a
-        software developer?
+        Throughout the decade, working in the broadcast industry has been an
+        amazing journey for developing my skills and my character. I was
+        introduced to daily graphics for news coverage and gradually navigated
+        into more demanding projects that tested my knowledge and creativity.
+        For instance, working with a team to create the current Hurricane
+        package and winning a silver Telly award for a 3D animation about winter
+        driving. I was eager to tinker with programming, but I didn't know where
+        or how I could apply my passion.
+        <br></br>
+        <br></br>I discovered the InTouch touchscreen system and I self-taught
+        JavaScript and React to develop apps for stations. At first, I was
+        overwhelmed, but I slowly pushed through and overcame many hurdles.
+        Learning a new language, new library, and manipulating elements with
+        only code to name a few. Eventually, I started building apps to help
+        cover topics such as the FIFA World Cup and the weather forecast. I
+        became very fluent with the InTouch system and my design expertise
+        blended perfectly.
+        <br></br>
+        <br></br>
+        Reflecting on my accomplishments now, I am enthusiastic to see what
+        doors open next in my career of design and programming.
       </p>
     </section>
   );
