fc5e58334cc5917b81e79be6b82819e498cc8e67
Patrick Wauschek <patrick@patrickwcode.com>
2024-11-11 19:06:29 -0600

    Adds Election Night App to Projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AchievementsPreview from "../media/steam-achievements-preview.png";
 import DreamGaragePreview from "../media/dream-garage-preview.PNG";
+import ElectionNight from "../media/election-night.png";
 import GitHubIcon from "../media/github-mark-white.svg";
 
 export default function Projects() {
@@ -11,7 +12,26 @@ export default function Projects() {
         className="py-[18vh] sm:grid sm:grid-cols-2 gap-4"
       >
         <h1 className="text-center text-4xl col-span-2">Projects</h1>
-        <div id="recorriendo-paris" className="sm:mr-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6">
+        <div
+          id="dream-garage-prototype"
+          className="sm:ml-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6"
+        >
+          <div id="election-night">
+            <img
+              alt="Election Night Touchscreen App"
+              id="election-night"
+              className="w-full aspect-[16/9] object-cover"
+              src={ElectionNight}
+            ></img>
+          </div>
+          <div id="achievements-header" className="text-2xl text-center">
+            2024 Election Night App
+          </div>
+        </div>
+        <div
+          id="recorriendo-paris"
+          className="sm:mr-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6"
+        >
           <div className="iframe-container overflow-hidden relative pt-[57%]">
             <iframe
               className="absolute top-0 right-0 bottom-0 left-0 w-full h-full"
@@ -26,9 +46,16 @@ export default function Projects() {
             Recorriendo Paris Open
           </div>
         </div>
-        <div id="dream-garage-prototype" className="sm:ml-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6">
+        <div
+          id="dream-garage-prototype"
+          className="sm:ml-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6"
+        >
           <div id="achievements-img">
-            <a href="https://www.figma.com/proto/5ruKeN0WUVzEqRfCEt0qc8/Dream-Garage---Prototype?node-id=57799-479&t=vEUcSZcECXqNKqyn-1" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.figma.com/proto/5ruKeN0WUVzEqRfCEt0qc8/Dream-Garage---Prototype?node-id=57799-479&t=vEUcSZcECXqNKqyn-1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img
                 alt="Achievements Preview"
                 id="achievements-img"
@@ -41,7 +68,10 @@ export default function Projects() {
             Dream Garage - Figma Prototype
           </div>
         </div>
-        <div id="react-demo-reel" className="sm:mr-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6">
+        <div
+          id="react-demo-reel"
+          className="sm:mr-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6"
+        >
           <div className="iframe-container overflow-hidden relative pt-[57%] pb-[-50%]">
             <iframe
               className="absolute top-0 right-0 bottom-0 left-0 w-full h-full"
@@ -56,7 +86,10 @@ export default function Projects() {
             React - InTouch Demo Reel
           </div>
         </div>
-        <div id="steam-achievements-project" className="sm:ml-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6">
+        <div
+          id="steam-achievements-project"
+          className="sm:ml-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6"
+        >
           <div
             id="live-icon"
             className="absolute ml-2 sm:ml-4 mt-4 inline-flex justify-center items-center bg-red-500 rounded-xl text-white w-20 border-2 border-red-300"
@@ -65,7 +98,11 @@ export default function Projects() {
             <div className="text-2xl">LIVE</div>
           </div>
           <div id="achievements-img">
-            <a href="https://achievements.patrickwcode.com/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://achievements.patrickwcode.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img
                 alt="Achievements Preview"
                 id="achievements-img"
@@ -90,7 +127,11 @@ export default function Projects() {
                   <img alt="GitHub Icon" className="" src={GitHubIcon}></img>
                 </div>
                 <div id="github-react">
-                  <a href="https://github.com/patrickwcode/SteamAchievementsReactJS" target="_blank" rel="noopener noreferrer">
+                  <a
+                    href="https://github.com/patrickwcode/SteamAchievementsReactJS"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     SteamAchievementsReactJS
                   </a>
                 </div>
@@ -100,7 +141,11 @@ export default function Projects() {
                   <img alt="GitHub Icon" className="" src={GitHubIcon}></img>
                 </div>
                 <div id="github-node">
-                  <a href="https://github.com/patrickwcode/steam-achievements-node" target="_blank" rel="noopener noreferrer">
+                  <a
+                    href="https://github.com/patrickwcode/steam-achievements-node"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     steam-achievements-node
                   </a>
                 </div>
@@ -108,7 +153,10 @@ export default function Projects() {
             </div>
           </div>
         </div>
-        <div id="graphics-demo-reel" className="sm:mr-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6">
+        <div
+          id="graphics-demo-reel"
+          className="sm:mr-4 mt-4 sm:mt-0 bg-white/60 rounded-3xl p-6"
+        >
           <div className="iframe-container overflow-hidden relative pt-[57%]">
             <iframe
               className="absolute top-0 right-0 bottom-0 left-0 w-full h-full"
